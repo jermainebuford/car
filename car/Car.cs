60ead264d1ace20b7a520a43375fe2145fc6a5d2
jermaine buford <bufordjermaine@outlook.com>
2022-06-10 12:11:18 -0400

    first init

---
+++ car/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace car
+{
+    public class Car
+    {
+        public Car (string make, string model, string year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+        public void PrintCar()
+        {
+            Console.WriteLine($"Make :  {Make}  Model :  {Model}  Year {Year}" );
+        }
+    }
+}
---
+++ car/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstCar = new Car("telsa", "model s", "2020" );
+            var secondcar = new Car("lexus", "mars", "2020");
+            var thirdcar = new Car("ford", "mustang", "2020");
+
+            firstCar.PrintCar();
+            secondcar.PrintCar();
+            thirdcar.PrintCar();
+        }
+    }
+}
